99d24abaa3ce407cf6635ee324221a0f8523b620
Bayazid Ahmed <169841598+Bayazid26@users.noreply.github.com>
2025-02-13 00:11:06 +0600

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int num;
+int num1;
+int sum;
+cout<< "Enter your number: ";
+cin>>num;
+
+cout<<"Enter your second number:";
+cin>>num1;
+sum=num+num1;
+cout<<"The sum of the two numbers is :"<<sum<<endl;
+return 0;
+}
